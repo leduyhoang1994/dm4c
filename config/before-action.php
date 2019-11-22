15e20b550652652b8e06962b262a2d97c40c4190
chungpb <phungbaochung@gmail.com>
2019-11-22 14:19:04 +0700

    check token

--- config/before-action.php
+++ config/before-action.php
@@ -2,8 +2,44 @@
 use app\models\User;
 use app\models\ApiToken;
 use cakebake\actionlog\model\ActionLog;
+
+function getAuthorizationHeader(){
+    $headers = null;
+    if (isset($_SERVER['Authorization'])) {
+        $headers = trim($_SERVER["Authorization"]);
+    }
+    else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+        $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+    } elseif (function_exists('apache_request_headers')) {
+        $requestHeaders = apache_request_headers();
+        // Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+        $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+        //print_r($requestHeaders);
+        if (isset($requestHeaders['Authorization'])) {
+            $headers = trim($requestHeaders['Authorization']);
+        }
+    }
+    return $headers;
+}
+/**
+ * get access token from header
+ * */
+function getBearerToken() {
+    $headers = getAuthorizationHeader();
+    // HEADER: Get the access token from the header
+    if (!empty($headers)) {
+        if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+            return $matches[1];
+        }
+    }
+    return null;
+}
+
 return function ($event) {
     $authKey = $_COOKIE['_identity'] ?? null;
+
+    $token = getBearerToken();
+    var_dump($token);die();
 
     $listControllerLog = [
         'cdt_list',
